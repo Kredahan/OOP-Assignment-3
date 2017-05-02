2f6e9a069dc1fd9f30ad4a607995bca8d87b04c9
Colm O'Reilly <C15502723@mydit.ie>
2017-05-02 19:42:54 +0100

    Scripts and images imported

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LevelManager : MonoBehaviour {
+
+    public Transform mainMenu, instructionsMenu, preferencesMenu;
+    bool muted;
+    [SerializeField]
+    Text mutetext;
+
+    public void LoadScene(string name)
+    {
+        Application.LoadLevel(name);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void InstructionsMenu (bool clicked)
+    {
+        if(clicked == true)
+        {
+            instructionsMenu.gameObject.SetActive(clicked);
+            mainMenu.gameObject.SetActive(false);
+        }else
+        {
+            instructionsMenu.gameObject.SetActive(clicked);
+            mainMenu.gameObject.SetActive(true);
+        }
+
+    }
+
+    public void PreferencesMenu (bool clicked)
+    {
+        if(clicked == true)
+        {
+            preferencesMenu.gameObject.SetActive(clicked);
+            mainMenu.gameObject.SetActive(false);
+        }else
+        {
+            preferencesMenu.gameObject.SetActive(clicked);
+            mainMenu.gameObject.SetActive(true);
+        }
+    }
+    
+    public void Mute()
+    {
+        muted = !muted;
+    }
+
+   void Update()
+    {
+        if (muted)
+        {
+            AudioListener.volume = 0;
+            mutetext.text = "Unmute";
+        }
+        else if (!muted)
+        {
+            AudioListener.volume = 1;
+            mutetext.text = "Mute";
+        }
+    }
+    
+}
