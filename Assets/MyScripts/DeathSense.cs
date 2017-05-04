c126c56d2ca3680e4c39db1cf5e25fe924fb20ab
Kevin Redahan <kevinredahan@gmail.com>
2017-05-04 11:41:10 +0100

    GameOver Screen

--- Assets/MyScripts/DeathSense.cs
+++ Assets/MyScripts/DeathSense.cs
@@ -6,11 +6,20 @@ public class DeathSense : MonoBehaviour
 {
     private void OnTriggerEnter(Collider other)
     {
-        //if (other.tag == "Player")
+        //if(other.tag == "Player")
         //{
            // GameObject.Find("Player").SendMessage("Die");
             Debug.Log("Player getting touched");
+            Time.timeScale = 0; //ending the game
+            Application.LoadLevel("GameOver");
        // }
 
     }
+
+   /* void GameOverScene(string name)
+    {
+        
+
+    }*/
 }
+
